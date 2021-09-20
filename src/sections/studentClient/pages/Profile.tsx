eac841a7ff4d146380f51dc0fab15226ec08075b
Eyuel <eyueltb@gmail.com>
2021-09-20 18:18:49 +0200

    Profile accept student interface-Eyuel

--- src/sections/studentClient/pages/Profile.tsx
+++ src/sections/studentClient/pages/Profile.tsx
@@ -8,32 +8,44 @@ import Links from "../components/profileComponents/LinkedIn";
 import DocFile from '../components/profileComponents/upLoad/DocFile';
 import PersBrev from '../components/profileComponents/upLoad/PersBrev';
 import Video from '../components/profileComponents/upLoad/Video';
-//import {Address} from  '../Interface/HandleInterface';
+import {Address, Person, Student} from '../Interface/HandleInterface';
 import {useStyles} from "../components/profileComponents/profileStyles/ProfileStyle";
 import Button from "@material-ui/core/Button";
-
-//It's
-interface Student {
-    name: string;
-    email: string;
-
-}
 
 const Profile: React.FC<Student> = () => {
 
     const classes = useStyles();
     const [studList, setStudList] = useState<Student>({
-        name: "Mjau",
-        email: "dd@bb.com",
+        person: {
+            fName: "Dawit",
+            lName: 'Tesfaye',
+            email: 'dawit@gmail.com',
+            phone:'123456',
+            address: {
+                street: 'da123',
+                postCode: '1234',
+                city: 'Stockholm'
+            },
+            photoLink: 'www.eyu.com/Eyu-Png',
+         },
+        linkedInLink:'www.linkedIn.com/Eyu',
+        school:'LNU',
+
     })
     return (
         <Grid container spacing={4} className={classes.root}>
             <Avatar src="/broken-image.jpg"/>
             <Paper elevation={3} style={{ width: 400, height: 400, background: 'white', }}>
-                <h1 className={classes.h1}> Profile </h1>
+                <h1 className={classes.h1}>Student Profile </h1>
                 <div className={classes.p}>
-                    <p >Name: {studList.name}</p>
-                    <p>Email: {studList.email}</p>
+                    <p >FName: {studList.person.fName}</p>
+                    <p >LName: {studList.person.fName}</p>
+                    <p>Email: {studList.person.email}</p>
+                    <p>Phone: {studList.person.phone}</p>
+                    <p>Street: {studList.person.address.street}</p>
+                    <p>City: {studList.person.address.city}</p>
+                    <p>School Name: {studList.school}</p>
+
                 </div>
                 <Button variant="contained" color="primary" component="span">
                  Edit
