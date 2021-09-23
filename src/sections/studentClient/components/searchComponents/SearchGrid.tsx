b6ed3aea0aab1fdcadf8b24557daa0f1cfa475b1
Gabriella Fry <frykhamnz@gmail.com>
2021-09-23 09:19:00 +0100

    WIP

--- src/sections/studentClient/components/searchComponents/SearchGrid.tsx
+++ src/sections/studentClient/components/searchComponents/SearchGrid.tsx
@@ -4,7 +4,8 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import {SearchField} from "./SearchField";
 import SearchList from "./SearchList";
-import Adds from '../mock-data/Adds';
+import Adds from "../../mock-data/Adds";
+import searchFunction from "../../utils/searchFunction";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -38,7 +39,7 @@ export default function SearchGrid() {
                 </Grid>
                 <Grid item xs={10}>
                     <Paper className={classes.paper}>
-                        {Adds.filter((advert) => searchFunction(advert, "title", query))
+                        {Adds.filter((advert) => searchFunction(advert, ["title", "description", "created", "updated"], query))
                             .map(advert => {
                             return (
                                 <h2>{advert.title}</h2>)
--- src/sections/studentClient/utils/searchFunction.ts
+++ src/sections/studentClient/utils/searchFunction.ts
@@ -1,11 +1,17 @@
 
 
-export default function searchFunction<T>(object: T, properties: Array<keyof T>, query: string): boolean {
+export default function searchFunction<T>(
+    object: T,
+    properties: Array<keyof T>,
+    query: string):
+    boolean {
+
     if (query === "") {
         return true;
     }
     return properties.some((property) => {
         const value = object[property];
+
         if (typeof value == "string") {
             return value.toString().toString().includes(query.toLocaleLowerCase())
         }
