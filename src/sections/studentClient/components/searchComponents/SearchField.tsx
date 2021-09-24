188491493c49c4048b741702665e5641ca7ee247
Gabriella Fry <frykhamnz@gmail.com>
2021-09-24 10:46:16 +0100

    created generic field for all components

--- src/sections/studentClient/components/searchComponents/SearchField.tsx
+++ src/sections/studentClient/components/searchComponents/SearchField.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import {Formik, Form, Field} from 'formik';
 import SearchButton from "./SearchButton";
+import {OurField} from "../sharedComponents/OurField";
 
 
 interface Values {
     searchString: string;
+    search2: string;
 }
 
 interface Props {
@@ -17,14 +19,20 @@ export const SearchField: React.FC<Props> = ({onSubmit}) => {
 
     return (
         <div>
-            <Formik initialValues={{searchString: ""}}
+            <Formik initialValues={{searchString: "", search2:""}}
                     onSubmit={(values) => {
                         onSubmit(values)
                     }}>
-                {({values, handleChange, handleBlur}) => (
+                {({values}) => (
                     <Form>
-                        <Field name="searchString"/>
+                        <Field name="searchString"
+                        component={OurField}/>
                         <SearchButton/>
+                        <Field
+                            name="search2"
+                            placeholder="search"
+                            component={OurField}
+                            />
                     </Form>
                 )}
             </Formik>
