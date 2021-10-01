cb6d4b0e76fc43c48d2bae07e5435b43e89b6ae9
chris <christian.sven.erik.myren@gmail.com>
2021-10-01 13:55:28 +0200

    LandingPage.tsx - Lagt till background image - Lagt till text - lagt styling på background image och text
    Behöver ordnas mer i stylingen både gällande
    background image och text.
    Samt stämma av om vi vill använda den text
    som jag lagt till nu eller om vi vill ändra
    eller ha något helt annat.
    Background image är också bara ett working
    example och kan bytas ut mot någon annan image
    eller skippas helt om vi så skulle vilja.
    
    Ett påbörjat förslag/utkast helt enkelt.
    
    /Christian

--- src/sections/studentClient/pages/LandingPage.tsx
+++ src/sections/studentClient/pages/LandingPage.tsx
@@ -1,9 +1,25 @@
 import React, {FC, useEffect, useState} from "react";
+import {Paper} from "@material-ui/core";
 
 const LandingPage:FC<{}> = () => {
     return (
-    <div>
-        <h1 style={{alignItems:'center', textAlign:'center'}}>Students Landing Page</h1>
+    <div style={{
+        backgroundImage: `url("https://images.pexels.com/photos/1629236/pexels-photo-1629236.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'auto',
+        color: 'white'}}>
+        <h1 style={{alignItems:'center', textAlign:'center'}}>Welcome to LiaLisera</h1>
+        <Paper style={{
+            background: 'transparent',
+            color: 'white',
+            paddingBottom: '5%',
+            paddingLeft: '5%',
+            paddingRight: '5%',
+            textAlign: 'center'
+        }}>
+            This is a professional platform made to help students and companies in their search for internships.<br/>
+            With the help of LiaLisera you can create and manage your own profile.
+        </Paper>
     </div>
     )
 
